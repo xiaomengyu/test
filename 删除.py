29a90eb85b242c012af874fd99529d9cd34691e3
xiaomengyu <39487142+xiaomengyu@users.noreply.github.com>
2018-05-22 08:40:08 +0800

    Add files via upload

---
+++ 删除.py
@@ -0,0 +1,44 @@
+# my_list=['j','hello',4785]
+# del my_list[8]
+# print(my_list)
+# # IndexError: list assignment index out of range下标错误：列表下标索引越界
+
+# my_list=['j','hello',4785]
+# del my_list[1]
+# print(my_list)
+
+# my_list=['j','hello',4785]
+# my_list.pop(1)
+# print(my_list)
+
+# my_list=['j','hello',4785]
+# my_list.pop(11)
+# print(my_list)
+
+# my_list=['j', 'hello', 4785, '48851', 'ghgf']
+# my_list.pop()
+# my_list.pop(0)
+# my_list.pop()
+# print(my_list)#pop()中为空默认删除最后一个
+
+# my_list=['j', 'hello', 4785, '48851', 'ghgf']
+# my_list.remove('ghgf')
+# print(my_list)
+
+# my_list=['j', 'hello', 4785, '48851', 'ghgf']
+# my_list.remove('gf')
+# print(my_list)
+
+# my_list=['j', 'hello', 4785, '48851', 'ghgf']
+# if 'hello'in my_list:
+#     index =my_list.index('hello')
+#     my_list.pop(index)
+# print(my_list)
+
+# my_list=['j', 'hello', 4785, '48851', 'ghgf']
+# if my_list.count('hello'):
+#     index = my_list.index('hello')
+#     my_list.pop(index)
+# print(my_list)
+
+
