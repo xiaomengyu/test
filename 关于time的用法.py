a663c8b9480418027b3ef81dc2583644eee28cf3
xiaomengyu <39487142+xiaomengyu@users.noreply.github.com>
2018-05-22 15:22:19 +0800

    Add files via upload

---
+++ 关于time的用法.py
@@ -0,0 +1,18 @@
+# Time    : 2018/5/22 15:18
+# Author  : Jack
+# File    : 关于time的用法.py
+# Software: PyCharm
+
+
+import time
+while True:
+    print('ok',time.time())
+
+
+
+
+
+
+
+
+
--- 草稿.py
+++ 草稿.py
@@ -5,8 +5,6 @@
 
 import this
 
-"fjfgkjfgkj"
-
 
 
 
