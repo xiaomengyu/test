e3d325811ba2c5c14568d9b34433686f82648319
xiaomengyu <39487142+xiaomengyu@users.noreply.github.com>
2018-12-28 20:22:24 +0800

    Add files via upload

---
+++ lianxi.py
@@ -0,0 +1,90 @@
+# Time    : 2018/12/28 10:11
+# Author  : Jack
+# File    : lianxi.py
+# Software: PyCharm
+
+
+# print('cccff')
+
+
+# num = 1
+# while num < 11:
+#     if num == 9:
+#         num += 1
+#         continue
+#     print(num)
+#     num += 1
+
+# msg = f'姓名:{name},年龄:{age}'
+# print(msg)
+
+# age = 66
+# count = 1
+# while count <= 3:
+#     count += 1
+#     guess_age = int(input(">>:"))
+#     if guess_age == age:
+#         print("yes,you got it...")
+#         count = 1
+#         continue
+#     elif guess_age < age:
+#         print('try heigher...')
+#     elif guess_age > age:
+#         print('try smeller...')
+#
+# print('你太笨了')
+
+
+# num = 1
+# while num <= 10:
+#     if num == 7:
+#         num = num + 1
+#         continue
+#
+#     print(num)
+#     num += 1
+
+
+# sum = 0
+# a = 1
+# while a <= 100:
+#     sum = sum + a
+#     a += 1
+# print(sum)
+
+
+# a = 1
+# while a <= 100:
+#     if a % 2 == 1:
+#         print(a)
+#     a += 1
+
+
+# a = 1
+# while a <= 100:
+#     if a % 2 == 0:
+#         print(a)
+#     a += 1
+
+# a = 1
+# sum = 0
+# while a <= 100:
+#     if a % 2 == 1:
+#         sum = sum + a
+#     if a % 2 == 0:
+#         sum = sum - a
+#     a += 1
+# print(sum)
+
+
+# count = 1
+# username = 'alex'
+# passwd = 123
+# while count <= 3:
+#     username1 = input('请输入你的用户名:')
+#     passwd1 = input('请输入你的通行密码:')
+#     if username1 == username and passwd == passwd1:
+#         print('欢饮你')
+#     else:
+#         print('invailed username or password,please try again....')
+#     count += 1
